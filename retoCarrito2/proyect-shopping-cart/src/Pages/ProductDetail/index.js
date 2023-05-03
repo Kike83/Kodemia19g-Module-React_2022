b17b2fde5dcc7efb8f2b5ad44ee5987b68415469
KikeBA <enrique.berdichevsky@gmail.com>
2023-05-03 12:09:53 -0600

    add reto-carrito-2

---
+++ retoCarrito2/proyect-shopping-cart/src/Pages/ProductDetail/index.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import ProductD from '../../Components/ProductDetail'
+
+const ProductDetail = ( {addHandler} ) => {
+    const [selectedProduct, setSelectedProduct] = useState({})
+    const { id } = useParams()
+    console.log(id)
+    useEffect(() => {
+        const getProductById = async () => {
+            let product = await fetch(`https://fakestoreapi.com/products/${id}`)
+            product = await product.json()
+            console.log(product)
+            setSelectedProduct(product)
+        }
+        getProductById()
+
+    }, [])
+    console.log(typeof selectedProduct)
+    
+    return (
+        <ProductD />
+
+    )
+}
+export default ProductDetail
---
+++ retoCarrito2/proyect-shopping-cart/src/Pages/Products/index.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react'
+
+import List from '../../Components/List'
+
+const Products = () => {
+    //useState para guardar array de objetos (productos)
+    const [productsData, setProductsData] = useState([])
+
+    //useEffect para traer data de servidor
+    useEffect(() => {
+        const getProducts = async () => {
+            let data = await fetch('https://fakestoreapi.com/products')
+            data = await data.json()
+            console.log('data', data)
+            setProductsData(data)
+        }
+        getProducts()
+
+
+    }, [])
+    //variable que contiene los datos del servidor
+    console.log('productsData', productsData)
+    return (
+        <><h1>Productos</h1>
+            <List data={productsData} />
+        </>)
+}
+export default Products
